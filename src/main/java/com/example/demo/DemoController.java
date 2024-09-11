54bde4edd7a61f16f1e05cb2ec3683f22d6c08f5
haaseoo <sojun1098@naver.com>
2024-09-11 14:37:59 +0900

    Controller

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DemoController {
+  @GetMapping("/hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "방가워요");
+    return "hello";
+  }
+}
